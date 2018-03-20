80269ec23582b951488537ac0d95ac2a12822d40
huyang8 <huyang8@jd.com>
2018-03-20 18:32:00 +0800

    番茄小程序前端界面提交

---
+++ tomatto/pages/about/about.js
@@ -0,0 +1,75 @@
+// pages/about/about.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    compName: '武汉番茄时代科技有限公司',
+    compMemos: [
+      '感谢您使用我们的“番茄管家”您在使用过程中有任何问题可以发送到我们的邮箱欢迎提出宝贵的意见！',
+      '关注我们的微信公众号：番茄管家TA（或搜索“fqgj520”）',
+      '邮箱：21131244@sina.com'
+    ],
+    compEnDesp: {
+      title: '公司简介',
+      desp: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.'
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- tomatto/pages/index/index.js
+++ tomatto/pages/index/index.js
@@ -4,51 +4,42 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    infomation: '番茄管家致力于为微商打造最方便的算账工具，下面是我们全部功能的介绍',
+    intros: [{
+      title: '初级功能',
+      items: [{
+        memo: '如何注册用户'
+      }, {
+        memo: '如何进行算账'
+      }, {
+        memo: '如何进行商品管理'
+      }, {
+        memo: '如何管理客户'
+      }, {
+        memo: '如何管理客户订单'
+      }]
+    }, {
+      title: '进阶功能',
+      items: [{
+        memo: '如何管理库存'
+      }, {
+        memo: '如何为下级代理分享商品'
+      }, {
+        memo: '如何管理库存'
+      }, {
+        memo: '如何为下级代理分享商品'
+      }, {
+        memo: '如何管理库存'
+      }, {
+        memo: '如何为下级代理分享商品'
+      }, {
+        memo: '如何管理库存'
+      }, {
+        memo: '如何为下级代理分享商品'
+      }]
+    }]
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
   }
 })
