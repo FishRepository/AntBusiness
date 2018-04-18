06dd668919a0b04f5feb9020b7ca6c1d5d2113fa
huyang8 <huyang8@jd.com>
2018-04-18 14:20:29 +0800

    番茄小程序界面样式调整

--- tomatto/pages/about/about.js
+++ tomatto/pages/about/about.js
@@ -13,7 +13,7 @@ Page({
     ],
     compEnDesp: {
       title: '公司简介',
-      desp: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.'
+      desp: '武汉番茄时代科技股份有限公司，是一家具有知名学院背景的互联网企业，专注于移动电商微商财务管理软件研发和行业生态圈服务发展的创新型企业。番茄时代创始人兼CEO徐祺先生，于2014年英国毕业回国，攻读管理学博士学位。同年带领一群海龟博士团队深入研究移动电商微商群体，4年多深入行业亲身经历，从微商小白发展成为万人团队长，看到一个个草根创业者，由小到大艰难发展过程，发现了制约微商创业者发展的痛点和需求，带着强烈的责任感与使命感，经过团队近3年的研发，于2018年正式推出全渠道与财务管理系统《番茄管家》APP，希望运用国内外先进的管理知识结合互联网科技为一体，为各大，中，小行业，品牌，商家提便携记账，进销存，客户管理等服务解决方案。助力微商移动电商行业快速发展。'
     }
   },
 
--- tomatto/pages/index/index.js
+++ tomatto/pages/index/index.js
@@ -7,17 +7,30 @@ Page({
   data: {
     infomation: '番茄管家致力于为微商打造最方便的算账工具，下面是我们全部功能的介绍',
     intros: [{
-      title: null,
+      title: '',
       items: []
     }]
   },
   onLoad: function () {
     var _this = this
+    wx.showLoading({
+      title: '',
+      mask: true
+    })
     wx.request({
       url: WebService.HOST + WebService.FETCH_INTRODUCTIONS_URL,
       success: function (result) {
+        wx.hideLoading();
         _this.setData({
           intros: result.data
+        })
+      },
+      fail:function(e){
+        wx.hideLoading();
+        wx.showToast({
+          title: '数据加载失败！',
+          icon: 'none',
+          duration: 2000
         })
       }
     })
