4f3196b6b3691d645a502bfe03e67d34f0d9ca54
qizhihang2017 <33796060+qizhihang2017@users.noreply.github.com>
2020-01-05 13:11:22 +0800

    客户新增字段更新

--- mapi/src/com/api/customer/entity/CustomerResult.java
+++ mapi/src/com/api/customer/entity/CustomerResult.java
@@ -3,11 +3,16 @@ package com.api.customer.entity;
 import com.api.common.utils.PropertiesUtil;
 import com.api.common.utils.StringUtil;
 
+import java.util.Date;
+
 public class CustomerResult{
 	
 	private Integer customer_id;
 	private String customer_username;
 	private String customer_icon;
+	private Date birthday;//生日
+	private String constellation;//星座
+	private Date period;//经期
 	
 	public Integer getCustomer_id() {
 		return customer_id;
@@ -34,5 +39,29 @@ public class CustomerResult{
 	}
 	public void setCustomer_icon(String customer_icon) {
 		this.customer_icon = customer_icon;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getConstellation() {
+		return constellation;
+	}
+
+	public void setConstellation(String constellation) {
+		this.constellation = constellation;
+	}
+
+	public Date getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(Date period) {
+		this.period = period;
 	}
 }
